e0b98a75ccaa48e173160e799e225bce2d074e65
Pablo <pablo@MacBook-Pro-de-Pablo.local>
2025-07-23 15:51:36 -0400

    Added some details

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
-export const API_URL = "http://192.168.0.104:3051";
+export const API_URL = "http://18.223.136.136:3041";
 export const GOOGLE_API_KEY ="AIzaSyCW710mvm2N7d2LD1YlgjIgyxKMiyP2E2I";
 //export const API_URL = "http://3.137.161.248:3041";
